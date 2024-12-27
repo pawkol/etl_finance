3d570afb1db053cd6781abac861f0b7bc6d8444d
chuck.nevada81 <chuck.nevada81@gmail.com>
2024-12-27 08:33:16 +0000

    new script

---
+++ airflow/scripts/schema.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+DB_CONFIG = {
+      "host" : "mysql_database",
+      "user" : "user1",
+      "password" : "user1",
+      "database" : "finance_data"
+}
+
+currency = {"PLN_to_USD", "PLN_to_EUR", "EUR_to_USD"}
+
+def tableCreation():
+    connection = mysql.connector.connect(**DB_CONFIG)
+    cursor = connection.cursor()
+
+    for x in currency:
+
+        cursor.execute(
+            f"""CREATE TABLE IF NOT EXISTS currency_data_{x}
+            (
+            id AUTO_INCREMENT PRIMARY KEY INT,
+            date DATE,
+            hour TIME,
+            exchange_rate FLOAT
+            )
+            """
+            )
+        
+    cursor.close()
+    connection.close()
+
+
+if __name__ == "__main__":
+    tableCreation()
